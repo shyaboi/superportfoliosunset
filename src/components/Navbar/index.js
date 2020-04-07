1ba11d275251a4b7487d3a16ea7351683596ff65
Ian <ian.shane.sears@gmail.com>
2020-04-06 23:56:30 -0700

    home button fresh

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./style.css";
+import Home from '../../imgs/home.png'
+
 
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <Link className="navbar-brand" to="/">
-        Home
+      <img id="homeButt" src={Home} alt="home"/>
       </Link>
       <div>
         <ul className="navbar-nav">
