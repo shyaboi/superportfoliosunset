593c1c66abe3c6e709244da8b696ac60c3bb2c7f
Ian <ian.shane.sears@gmail.com>
2020-04-07 12:11:23 -0700

    youtube added and new gif....might be overload

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -10,7 +10,7 @@ import poke from '../../imgs/poke.gif'
 import namegen from '../../imgs/namegen.gif'
 import glow from '../../imgs/glow.png'
 import adobe from '../../imgs/lidl.jpg'
-
+import mp from '../../imgs/mp.gif'
 // import { BrowserRouter as Router, Route } from "react-router-dom";
 // import Contact from './pages/Contact'
 
@@ -25,18 +25,21 @@ function Main() {
       <header className="App-header">
     <section class="animated-grid">
  {/* <Route exact path="/Contact" component={Contact} /> */}
-        <div class="card border-gradient border-gradient-purple">  <a href="https://stock.adobe.com/contributor/209064146/Ian"><p id='desc'>Adobe Stock</p>
-        <img src={adobe} alt="Dawn of weather"/></a></div>
+ <div class="card border-gradient border-gradient-purple">  <a href="https://www.youtube.com/channel/UCkTSBEk8wsKIpy-MzRoJroA"><p id='desc'>MaiLPoem YouTube</p>
+        <img src={mp} alt="mail poem youtube"/></a></div>
+
+        <div class="card border-gradient border-gradient-purple">  <a href="https://stock.adobe.com/contributor/209064146/Ian"><p id='desc'>Adobe Stock Photo Portfolio</p>
+        <img src={adobe} alt="Adobe Stock"/></a></div>
 
 
-    <div class="card border-gradient border-gradient-purple">  <a href="https://shyaboi.github.io/TubularRepoNameGeneratorSoldier/"><p id='desc'>Repo Name Gen</p>
+    <div class="card border-gradient border-gradient-purple">  <a href="https://shyaboi.github.io/TubularRepoNameGeneratorSoldier/"><p id='desc'>Semi-Random Repo Name Gen</p>
         <img src={namegen} alt="Name Generator"/></a></div>
 
 
     <div class="card border-gradient border-gradient-purple">  <a href="https://lorddominic.github.io/grocerEASE"><p id='desc'>grocerEASE</p>
         <img src={groce} alt="Grocer Ease"/></a></div>
 
-    <div class="card border-gradient border-gradient-purple">  <a href="https://shyaboi.github.io/glowingScheduler"><p id='desc'>Workday scheduler</p>
+    <div class="card border-gradient border-gradient-purple">  <a href="https://shyaboi.github.io/glowingScheduler"><p id='desc'>8 Hour Workday scheduler</p>
 <img src={glow} alt="glowing scheduler"/></a></div>
 
       <div class="card border-gradient border-gradient-purple">  <a href="https://github.com/shyaboi"><p id='desc'>Github</p>
