24d9de64caed6d37785e5c0ee39d3663c511e480
Ian <ian.shane.sears@gmail.com>
2020-04-21 17:36:39 -0700

    took away link style on contactt

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,32 +1,39 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./style.css";
-import Home from '../../imgs/home.png'
-
+import Home from "../../imgs/home.png";
 
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 function Navbar() {
   return (
-    <nav id='nav' className="navbar navbar-expand-lg navbar-light bg-light">
-        <div id="homeButt" >
-      <Link className="navbar-brand" to="/">
-      <img src={Home} alt="home"/>
-      </Link>
+    <nav id="nav" className="navbar navbar-expand-lg navbar-light bg-light">
+      <div id="homeButt">
+        <Link className="navbar-brand" to="/">
+          <img src={Home} alt="home" />
+        </Link>
       </div>
       <div className="navbar-nav">
         <div>
-        <ul>
-       
-          <li className="nav-item" id="contactButt">
-            <Link
-              to="/Contact"
-              className={window.location.pathname === "/Contact" ? "nav-link active" : "nav-link"}
+          <ul style={{ textDecoration: "none" }}>
+            <li
+              className="nav-item"
+              id="contactButt"
+              style={{ textDecoration: "none" }}
             >
-              Contact
-            </Link>
-          </li>
-        </ul>
-      </div>
+              <Link
+              style={{ color: 'inherit', textDecoration: 'inherit'}}
+                to="/Contact"
+                className={
+                  window.location.pathname === "/Contact"
+                    ? "nav-link active"
+                    : "nav-link"
+                }
+              >
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
