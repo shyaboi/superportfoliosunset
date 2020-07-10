da39955dcf7fdc65d5c3243052b5f15dcfeb1d7b
Shyaboi <ian.shane.sears@gmail.com>
2020-07-10 01:00:37 -0700

    resized p inside hover box

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -31,27 +31,27 @@ function Main() {
   function mpAppDescription() {
     var elem = document.getElementById("bigboi");
     elem.innerHTML =
-      "<div id='newMidDiv'><h1>MaiL Poem</h1> <br> <p>MaiL Poem is an art project ment to blend music loops with ambient sounds and a vibrant story.</p></div>";
+      "<div id='newMidDiv'><h1>MaiL Poem</h1> <br> <p>MaiL Poem is an art project ment to blend music loops with ambient sounds and a vibrant story. It is currently still a work in progress</p></div>";
   }
   function adobeStock() {
     var elem = document.getElementById("bigboi");
     elem.innerHTML =
-      "<div id='newMidDiv'><h1>Adobe Stock</h1> <br> <p>This is muh adobe stock portfolio</div>";
+      "<div id='newMidDiv'><h1>Adobe Stock</h1> <br> <p>This is my adobe stock portfolio. Feel free to browse and dowload my work on Adobe Stock</div>";
   }
   function mpYouTube() {
     var elem = document.getElementById("bigboi");
     elem.innerHTML =
-      "<div id='newMidDiv'><h1>YouTube</h1> <br> <p>This Is muh YouTube</div>";
+      "<div id='newMidDiv'><h1>YouTube</h1> <br><div> <p>This is my YouTube where I have only posted a couple timelapses and a cinemagraph</div></div>";
   }
   function gitHub() {
     var elem = document.getElementById("bigboi");
     elem.innerHTML =
-      "<div id='newMidDiv'><h1>GitHub</h1> <br> <p>This Is muh GitHub</div>";
+      "<div id='newMidDiv'><h1>GitHub</h1> <br> <p>This is my GitHub. Take a look at some of the source code that makes up my projects. Please give me and feedback if you have it. It would be greatly appreciated.</div>";
   }
   function linked() {
     var elem = document.getElementById("bigboi");
     elem.innerHTML =
-      "<div id='newMidDiv'><h1>LinkedIn</h1> <br> <p>This Is muh LinkedIn</div>";
+      "<div id='newMidDiv'><h1>LinkedIn</h1> <br> <p>Please connect with me on linkedIn. I am currently open to new oppertunies! </div>";
   }
   function grocerEase() {
     var elem = document.getElementById("bigboi");
@@ -61,17 +61,17 @@ function Main() {
   function nameGen() {
     var elem = document.getElementById("bigboi");
     elem.innerHTML =
-      "<div id='newMidDiv'><h1>Repo Name Generator</h1> <br> <p>This applicatioin randomly generates a repo name from 2 given name arrays.</div>";
+      "<div id='newMidDiv'><h1>Repo Name Generator</h1> <br> <p>This applicatioin randomly generates a repo name from 2 given name arrays. I plan on making different repo version settings, i.e. create-react-app wants repos to be all lowercase/</div>";
   }
   function workWork() {
     var elem = document.getElementById("bigboi");
     elem.innerHTML =
-      "<div id='newMidDiv'><h1>8 Hour Workday Scheduler</h1> <br> <p>This is a simple sceduler that will save your notes for each hour of your work day.</div>";
+      "<div id='newMidDiv'><h1>8 Hour Workday Scheduler</h1> <br> <p>This is a simple sceduler that will save your notes for each hour of your work day. It will save the text in the hour to the internal storage in your browser, so currently to clear your schedule, clear your browsers internal storage.</div>";
   }
   function jQ() {
     var elem = document.getElementById("bigboi");
     elem.innerHTML =
-      "<div id='newMidDiv'><h1>JQuery Weather</h1> <br> <p>This application can take an input of any city in open weathers DB with a fetch API and give current weather with a UV index, wind speed, and humidity, as well as a five day forcast.</div>";
+      "<div id='newMidDiv'><h1>JQuery Weather</h1> <br> <p>This application can take an input of any city in open weathers DB with a fetch API and give current weather with a UV index, wind speed, and humidity, as well as a five day forcast. This application is made only with HTML, Javascript, and JQuery</div>";
   }
 
   function pokemon() {
@@ -82,7 +82,7 @@ function Main() {
   function muhWalls() {
     var elem = document.getElementById("bigboi");
     elem.innerHTML =
-      "<div id='newMidDiv'><h1>MuhWalls</h1> <br> <p>I always spent tons of time on wallpaper websites picking hundereds of wallpapers to add to my slideshow of nice backrounds. Now i have finally made my own wallpaper site. Currently you can upload and print all the uploaded wallpapers in a lovely grid. I currently have the upload server seperate and offile.</p>(this app is currently hosted on heroku, whos servers get put to 'sleep' so give it a minuet to load up)</div>";
+      "<div id='newMidDiv'><h1>MuhWalls</h1><p>I always spent tons of time on wallpaper websites picking hundereds of wallpapers to add to my wallpaper folder. Now i have finally made my own wallpaper site. Currently the site can upload images and print all the uploaded wallpapers in a lovely grid. I currently have the upload server seperate and offline. </p><br>(this app is currently hosted on heroku, whos servers get put to 'sleep' so give it a minuet to load up)";
   }
   return (
     <div className="App">
@@ -208,8 +208,11 @@ function Main() {
             id="bigboi"
             className="card border-gradient border-gradient-purp"
           >
+              <div>
             <h1>Ian Sears</h1>
             <h3>Full Stack Web Computer Man</h3>
+            </div>
+            <Video />
           </div>
         </section>
       </header>
