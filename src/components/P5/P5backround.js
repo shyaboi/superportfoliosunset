c7db6ccd2276fa6e1a0a1a066e7e51b5e012172d
Shyaboi <ian.shane.sears@gmail.com>
2020-11-02 17:30:58 -0700

    backround resizing with window

--- src/components/P5/P5backround.js
+++ src/components/P5/P5backround.js
@@ -1,38 +1,37 @@
 import React from "react";
 import Sketch from "react-p5";
- 
+ let windowResized;
  const P5sketch =  (props) => {
-  var canvas;
-  var c, c1, c2;
-  var r=0,g=0,b=0;
-  var bigRan = Math.floor(Math.random(1)*3)
+  let canvas;
+  let r=0,g=0,b=0;
+  let bigRan = Math.floor(Math.random(1)*3)
   
     const setup = (p5, canvasParentRef) => {
-        // use parent to render the canvas in this ref
-        // (without that p5 will render the canvas outside of your component)
         canvas = p5.createCanvas(p5.windowWidth, p5.windowHeight).parent(canvasParentRef);
         canvas.position(0,0);
         canvas.style('z-index', '-1')
-        // Define colors
         switch (bigRan) {
         case 0: r=255; break;
         case 1: g=255; break;
         case 2: b=255; break;
         }
-            // g=ranG;
-            // b=ranB;
+
+         windowResized = () =>{
+          p5.resizeCanvas(p5.windowWidth, p5.windowHeight)  
+        }
+    
         const rgb = ()=> {
             
-                console.log(r,g,b)
-              if(r > 0 && b == 0){
+                // console.log(r,g,b)
+              if(r > 0 && b === 0){
                 r--;
                 g++;
               }
-              if(g > 0 && r == 0){
+              if(g > 0 && r === 0){
                 g--;
                 b++;
               }
-              if(b > 0 && g == 0){
+              if(b > 0 && g === 0){
                 r++;
                 b--;
               }
@@ -48,7 +47,7 @@ import Sketch from "react-p5";
 
 
     const draw = (p5) => {
-       
+        windowResized()
             p5.background(r,g,b);
     };
  
