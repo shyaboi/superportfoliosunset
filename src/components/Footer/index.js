fb2c31c790b36cbc10e0765d510b4335077d1df6
Shyaboi <ian.shane.sears@gmail.com>
2020-11-14 23:51:01 -0700

    added wordpress site in footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,6 +5,7 @@ import linkedIn from "../../imgs/linked.png";
 import youTube from "../../imgs/youtube.png";
 import adobe from "../../imgs/adobe.png";
 import cv from "../../imgs/cv.png";
+import iswp from "../../imgs/wordpress.png";
 
 
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
@@ -13,9 +14,16 @@ function Footer() {
     <div className="footer">
     
     <a href="https://github.com/shyaboi"><img src={github} id='gitHub' alt='github logo'/></a>
+
     <a href="https://www.linkedin.com/in/ian-sears-5931734a" ><img src={linkedIn} id='linkedIn' alt='linkedIn logo'/></a>
+
+    <a href="https://ian-sears.com/" ><img src={iswp} id='iswp' alt='ian sears wordpress'/></a>
+    
     <a href="https://www.youtube.com/channel/UCkTSBEk8wsKIpy-MzRoJroA?view_as=subscriber" ><img src={youTube} id='youTube' alt='youtube logo'/></a>
+
+
     <a href="https://stock.adobe.com/contributor/209064146/Ian" ><img src={adobe} id='adobe' alt='adobe logo'/></a>
+
     <a href="https://docs.google.com/document/d/1RnXMgfoLe12ix2YpVzLk9Olue4SjtuMb331qsIks14Y/edit?usp=sharing" ><img src={cv} id='cv' alt='curriculum vitae'/></a>
 
 
